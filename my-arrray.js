57d9b323a4b608d167cb27a58c7fe350e1a667e5
Withawat Phiraban <withawat1201@gmail.com>
2026-07-02 13:39:37 +0700

    add array and object to webpage

---
+++ my-arrray.js
@@ -0,0 +1,3 @@
+let food = ["pizza","mala tang" , "hamburger", "hot dog", "sushi", "tacos", "pasta", "salad", "ice cream", "chocolate"];
+
+console.log(food)
---
+++ my-object.js
@@ -0,0 +1,92 @@
+let characterDatabase = [
+  {
+    id: 1,
+    name: "Arthur Pendragon",
+    class: "Paladin",
+    level: 50,
+    hp: 2500,
+    skills: ["Holy Shield", "Justice Strike"],
+    isAlive: true
+  },
+  {
+    id: 2,
+    name: "ShadowBlade",
+    class: "Assassin",
+    level: 45,
+    hp: 1200,
+    skills: ["Stealth", "Double Strike", "Poison Dart"],
+    isAlive: true
+  },
+  {
+    id: 3,
+    name: "Jaina Proudmoore",
+    class: "Mage",
+    level: 48,
+    hp: 1100,
+    skills: ["Blizzard", "Teleport", "Mana Shield"],
+    isAlive: true
+  },
+  {
+    id: 4,
+    name: "Gorgoroth",
+    class: "Berserker",
+    level: 52,
+    hp: 3100,
+    skills: ["Blood Rage", "Whirlwind"],
+    isAlive: false
+  },
+  {
+    id: 5,
+    name: "Legolas Greenleaf",
+    class: "Archer",
+    level: 42,
+    hp: 1400,
+    skills: ["Multi-Shot", "Eagle Eye"],
+    isAlive: true
+  },
+  {
+    id: 6,
+    name: "Eldrin the Wise",
+    class: "Cleric",
+    level: 40,
+    hp: 1600,
+    skills: ["Heal", "Resurrection", "Bless"],
+    isAlive: true
+  },
+  {
+    id: 7,
+    name: "Vipera",
+    class: "Necromancer",
+    level: 47,
+    hp: 1300,
+    skills: ["Raise Dead", "Soul Drain"],
+    isAlive: true
+  },
+  {
+    id: 8,
+    name: "Bruce",
+    class: "Monk",
+    level: 44,
+    hp: 1900,
+    skills: ["Dragon Kick", "Meditate"],
+    isAlive: true
+  },
+  {
+    id: 9,
+    name: "Sauron's Hand",
+    class: "Death Knight",
+    level: 55,
+    hp: 2800,
+    skills: ["Death Grip", "Unholy Aura"],
+    isAlive: false
+  },
+  {
+    id: 10,
+    name: "Valerie",
+    class: "Valkyrie",
+    level: 49,
+    hp: 2200,
+    skills: ["Spear Throw", "Heavenly Grace"],
+    isAlive: true
+  }
+];
